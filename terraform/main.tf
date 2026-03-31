bfe76f6879e77eb8d66e7d5e781d3a88fd73f376
SumanthMaddali <sumanthmaddali01@gmail.com>
2026-04-01 01:39:41 +0530

    chore: fix gitignore and add vpc networking resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,55 @@ resource "aws_key_pair" "devops_key" {
   key_name = var.key_name
   public_key = file(var.publick_key_path)
 }
+
+#VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+#Public subnet
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devops-public-subnet"
+  }
+}
+
+#Intenet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "devops-igw"
+  }
+}
+
+#Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "devops-public-rt"
+  }
+}
+
+#Associate route table with subnet
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
